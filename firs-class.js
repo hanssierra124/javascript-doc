efa78c406afc822e09a749a15a1819e45868cf44
hanssierra <hans.sierra@unisimon.edu.co>
2024-12-05 22:51:46 -0500

    primer archivo

---
+++ firs-class.js
@@ -0,0 +1,20 @@
+let cajaDeAndy = 'hola';
+console.log(cajaDeAndy)
+
+// nombre de variable legible
+
+// let algo que cambie ejemplo un contador
+// const variables que son fijas
+// 10 tipos de datos
+// primitivos
+
+//PRIMITIVOS
+
+//string
+let nombre = 'tere';
+// number
+let edad = '25';
+//boolean
+let esMayorDeEdad = true;
+// null
+let nohayvalor = null;
